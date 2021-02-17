8ac8322bfdd6881d166955ec880ad5a41a10c29c
Ann-oss <anyapristavka@gmail.com>
2021-02-17 08:42:05 +0300

    add loading toys

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -9,15 +9,15 @@ export const Loading = () => {
                 className='toys-block'
                 speed={2}
                 width={280}
-                height={460}
-                viewBox="0 0 280 460"
+                height={424}
+                viewBox="0 0 280 424"
                 backgroundColor="#f3f3f3"
-                foregroundColor="#ecebeb">
-                <circle cx="132" cy="142" r="115" />
-                <rect x="0" y="273" rx="6" ry="6" width="280" height="26" />
-                <rect x="0" y="310" rx="6" ry="6" width="280" height="84" />
-                <rect x="0" y="418" rx="6" ry="6" width="91" height="31" />
-                <rect x="137" y="408" rx="25" ry="25" width="140" height="46" />
+                foregroundColor="#ecebeb"
+
+            >
+                <rect x="9" y="1" rx="5" ry="5" width="260" height="260" /> 
+                <rect x="3" y="292" rx="4" ry="4" width="280" height="44" /> 
+                <rect x="0" y="354" rx="4" ry="4" width="280" height="44" />
             </ContentLoader>
         </div>
     );
